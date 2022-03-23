b0e4de723308cf73cbf646db715711c8d18cf3ff
Ab-Ly <96085607+Ab-Ly@users.noreply.github.com>
2022-03-23 16:52:17 +0100

    Create: F.Item / F.List / Header / Global Style / React-Icons

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import PropTypes from "prop-types";
+import Card from "./Shared/Card";
+
+function FeedbackItem({ item }) {
+  return (
+    <Card>
+      <div className="num-display">{item.rating}</div>
+      <button className="close">
+        <FaTimes color="purple" />
+      </button>
+      <div className="text-display">{item.text}</div>
+    </Card>
+  );
+}
+
+FeedbackItem.propTypes = { item: PropTypes.object.isRequired };
+
+export default FeedbackItem;
